4ca1062adccefaea19fb9c73f11dc871522ecd74
Fedai Kaya <codelovesme@gmail.com>
2021-05-01 16:08:07 +0300

    Fix ci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for entry in "packages"
+for entry in packages/*
 do
-  echo "//codeloves.me:2800/api/v4/projects/$1/packages/npm/:_authToken=$2">packages/$entry/.npmrc
-  npm publish "packages/$entry"
+  echo "//codeloves.me:2800/api/v4/projects/$1/packages/npm/:_authToken=$2">$entry/.npmrc
+  npm publish $entry
 done
