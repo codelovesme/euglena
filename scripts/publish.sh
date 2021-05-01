ef83c966bba310fe6c4452589223b35c54fc7637
Fedai Kaya <codelovesme@gmail.com>
2021-05-01 16:58:28 +0300

    sdsds

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ for entry in packages/*
 do
   cd $entry
   echo "//codeloves.me:2800/api/v4/projects/$1/packages/npm/:_authToken=$2">.npmrc
-  cat .npmrc
+  cat ${#2}
   npm publish
   cd ../..
 done
