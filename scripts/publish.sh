1dfd08021e9c6e90e58bda159daaab4ce625c750
Fedai Kaya <codelovesme@gmail.com>
2022-01-09 20:59:09 +0300

    Update gitlab-ci  & package json publish script & Fix package script version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,8 +3,7 @@
 for entry in packages/*
 do
   cd $entry
-  echo "//codeloves.me:2800/api/v4/projects/$1/packages/npm/:_authToken=$2">.npmrc
-  cat ${#2}
+  echo "//registry.npmjs.org/:_authToken=$1" > .npmrc
   npm publish
   cd ../..
 done
