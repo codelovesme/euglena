de37fa7b52180fb80138c5ca50cb3931917a2cb9
Fedai Kaya <fedai@codeloves.me>
2022-10-19 14:30:42 +0200

    Rename update-euglena-deps

---
+++ scripts/update-euglena-deps.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+const { argv } = require("node:process");
+const { join } = require("path");
+const { readdirSync,readFileSync, statSync, writeFileSync } = require("fs");
+const beautify = require("json-beautify");
+
+/**
+ * TODO: Move this function into cessanlib.nodejs
+ */
+const getAllFiles = (dir, fileName, forbiddenFolders=[]) => {
+    console.log(`Directory: ${dir}`);
+    const items = readdirSync(dir);
+    let files = [];
+    for (const item of items) {
+        console.log(`Check if item ${item} is directory or file`);
+        const itemPath = join(dir, item);
+        if (statSync(itemPath).isDirectory() && !forbiddenFolders.includes(item)) {
+            try {
+                files = files.concat(getAllFiles(itemPath,fileName, forbiddenFolders));
+            } catch (error) {
+                console.log(`Error occurred while reading dir: ${item}`);
+                continue;
+            }
+        } else if (item.endsWith(fileName)) {
+            files.push(itemPath);
+        }
+        console.log(`Item ${item} is nor a directory or a package.json file`);
+    }
+    return files;
+}
+
+const directory = argv[2];
+const version = argv[3];
+
+console.log("Updating euglena related dependency versions");
+console.log(`Directory working in: ${directory}`)
+console.log(`Version to set: ${version}`)
+console.log("Finding package.json files")
+const files = getAllFiles(directory,"package.json",["node_modules"]);
+console.log(`Total found package.json files: ${files.length}`)
+
+const updateDependencies = (dependencies) => {
+    if(!dependencies) return dependencies;
+    const packageNames = Object.keys(dependencies);
+    const euglenaRelatedPackageNames = packageNames.filter(key => key.startsWith("@euglena/"));
+    return euglenaRelatedPackageNames.reduce((acc,curr)=>{
+        console.log(`Changing dependency ${curr} version from ${acc[curr]} to ^${version}`);
+        return{
+            ...acc,
+            [curr]: `^${version}`
+        }
+    },{...dependencies});
+}
+
+const updateAllRelatedDependencies = (packageJson, dependenciesArr) => dependenciesArr.reduce((acc,curr)=>{
+    console.log(`Updating packages under ${curr}`);
+    return {
+        ...acc,
+        [curr]: updateDependencies(acc[curr])
+    }
+},{...packageJson});
+
+console.log("Start updating...")
+for(const file of files) {
+    console.log(`Start working on ${file}`);
+    try{
+        const jsonString = readFileSync(file);
+        const packageJson = JSON.parse(jsonString);
+        const dependenciesArr = ["dependencies","peerDependencies","devDependencies"]; 
+        const newPackageJson = updateAllRelatedDependencies(packageJson, dependenciesArr);
+        console.log(`Writing file ${file}`);
+        writeFileSync(file, beautify(JSON.stringify(newPackageJson)));
+    } catch(e) {
+        console.log(`Error occurred while working on ${file} error: ${e.message}`);
+    }
+}
+
