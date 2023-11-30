4f26e2001f77e8bc96298fa1519dc54d3c86d1e7
fedai <fedai@codeloves.me>
2023-12-01 00:07:13 +0100

    Update compact.react

--- packages/compact.react/src/index.tsx
+++ packages/compact.react/src/index.tsx
@@ -1,172 +1,27 @@
-import { Particle, State } from "@euglena/compact";
-import React, { createContext, useState } from "react";
+import { State, ui } from "@euglena/compact";
+import React, { createContext, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { register, unregister } from "./serviceWorker";
 
-const createServiceWorker = () => {
-    // This optional code is used to register a sergvice worker.
-    // register() is not called by default.
+export const ToolsContext = createContext({} as { fireEvent: ui.Nucleus["handleEvent"] });
 
-    // This lets the app load faster on subsequent visits in production, and gives
-    // it offline capabilities. However, it also means that developers (and users)
-    // will only see deployed updates on subsequent visits to a page, after all the
-    // existing tabs open on the page have been closed, since previously cached
-    // resources are updated in the background.
+let setState: (state: any) => void;
 
-    // To learn more about the benefits of this model and instructions on how to
-    // opt-in, read https://bit.ly/CRA-PWA
-
-    const isLocalhost = Boolean(
-        window.location.hostname === "localhost" ||
-        // [::1] is the IPv6 localhost address.
-        window.location.hostname === "[::1]" ||
-        // 127.0.0.1/8 is considered localhost for IPv4.
-        window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-    );
-
-    type Config = {
-        onSuccess?: (registration: ServiceWorkerRegistration) => void;
-        onUpdate?: (registration: ServiceWorkerRegistration) => void;
-    };
-
-
-
-    function registerValidSW(swUrl: string, config?: Config) {
-        navigator.serviceWorker
-            .register(swUrl)
-            .then((registration) => {
-                registration.onupdatefound = () => {
-                    const installingWorker = registration.installing;
-                    if (installingWorker == null) {
-                        return;
-                    }
-                    installingWorker.onstatechange = () => {
-                        if (installingWorker.state === "installed") {
-                            if (navigator.serviceWorker.controller) {
-                                // At this point, the updated precached content has been fetched,
-                                // but the previous service worker will still serve the older
-                                // content until all client tabs are closed.
-                                console.log(
-                                    "New content is available and will be used when all " +
-                                    "tabs for this page are closed. See https://bit.ly/CRA-PWA."
-                                );
-
-                                // Execute callback
-                                if (config && config.onUpdate) {
-                                    config.onUpdate(registration);
-                                }
-                            } else {
-                                // At this point, everything has been precached.
-                                // It's the perfect time to display a
-                                // "Content is cached for offline use." message.
-                                console.log("Content is cached for offline use.");
-
-                                // Execute callback
-                                if (config && config.onSuccess) {
-                                    config.onSuccess(registration);
-                                }
-                            }
-                        }
-                    };
-                };
-            })
-            .catch((error) => {
-                console.error("Error during service worker registration:", error);
-            });
-    }
-
-    function checkValidServiceWorker(swUrl: string, config?: Config) {
-        // Check if the service worker can be found. If it can't reload the page.
-        fetch(swUrl)
-            .then((response) => {
-                // Ensure service worker exists, and that we really are getting a JS file.
-                const contentType = response.headers.get("content-type");
-                if (response.status === 404 || (contentType != null && contentType.indexOf("javascript") === -1)) {
-                    // No service worker found. Probably a different app. Reload the page.
-                    navigator.serviceWorker.ready.then((registration) => {
-                        registration.unregister().then(() => {
-                            window.location.reload();
-                        });
-                    });
-                } else {
-                    // Service worker found. Proceed as normal.
-                    registerValidSW(swUrl, config);
-                }
-            })
-            .catch(() => {
-                console.log("No internet connection found. App is running in offline mode.");
-            });
-    }
-
-
-
-    return {
-        register(config?: Config) {
-            if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-                // The URL constructor is available in all browsers that support SW.
-                const publicUrl = new URL((process as { env: { [key: string]: string } }).env.PUBLIC_URL, window.location.href);
-                if (publicUrl.origin !== window.location.origin) {
-                    // Our service worker won't work if PUBLIC_URL is on a different origin
-                    // from what our page is served on. This might happen if a CDN is used to
-                    // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-                    return;
-                }
-
-                window.addEventListener("load", () => {
-                    const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-                    if (isLocalhost) {
-                        // This is running on localhost. Let's check if a service worker still exists or not.
-                        checkValidServiceWorker(swUrl, config);
-
-                        // Add some additional logging to localhost, pointing developers to the
-                        // service worker/PWA documentation.
-                        navigator.serviceWorker.ready.then(() => {
-                            console.log(
-                                "This web app is being served cache-first by a service " +
-                                "worker. To learn more, visit https://bit.ly/CRA-PWA"
-                            );
-                        });
-                    } else {
-                        // Is not localhost. Just register service worker
-                        registerValidSW(swUrl, config);
-                    }
-                });
-            }
-        },
-        unregister() {
-            if ("serviceWorker" in navigator) {
-                navigator.serviceWorker.ready.then((registration) => {
-                    registration.unregister();
-                });
-            }
-        }
-    }
-
-}
-
-export interface Nucleus {
-    handleEvent(event: Particle): void;
-}
-export const ToolsContext = createContext({} as { fireEvent: Nucleus["handleEvent"] });
 export class UIReact {
-    private setState?: (state: any) => void;
-    constructor(
-        private rootComponent: React.FC<any>,
-        private serviceWorker: boolean,
-        private nucleus: Nucleus
-    ) {
-        const { register, unregister } = createServiceWorker();
-        (this.serviceWorker ? register : unregister)();
+    constructor(rootComponent: React.FC<any>, serviceWorker:boolean, nucleus: ui.Nucleus) {
+        (serviceWorker ? register : unregister)();
         const root = createRoot(document.getElementById("root")!);
         const App = () => {
-            const [state, _setState] = useState(new State());
-            this.setState = _setState;
-            return this.rootComponent(state);
+            const [state, _setState] = useState(undefined);
+            setState = _setState;
+            useEffect(() => {
+                console.log('React render completed');
+             }, []);
+            return rootComponent(state);
         };
-        root.render(<ToolsContext.Provider value={{ fireEvent: this.nucleus.handleEvent }}> {< App />}</ToolsContext.Provider>);
+        root.render(<ToolsContext.Provider value={{ fireEvent: nucleus.handleEvent }}>{<App />}</ToolsContext.Provider>);
     }
-
-    render(state: any) {
-        this.setState!(state);
+    render (state:State) {
+        setState?.(state);
     }
 }
