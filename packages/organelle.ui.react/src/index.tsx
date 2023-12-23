5de610fcced1bbdbcbc07f49bc789859db3eb6e6
fedai <fedai@codeloves.me>
2023-12-23 20:11:20 +0100

    Fix organelle.ui.react

--- packages/organelle.ui.react/src/index.tsx
+++ packages/organelle.ui.react/src/index.tsx
@@ -20,8 +20,7 @@ export const ToolsContext = createContext({
         ((class_: "Event") => sys.io.ui.Event)
 });
 
-let setState: (state: any) =
-> void;
+let setState: (state: any) => void;
 
 export default dco<sys.io.ui.UI, Sap>({
     Sap: async (p, { t }) => {
